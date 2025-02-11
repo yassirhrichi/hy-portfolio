ff2fe4327b17016adc824b37acafcf1041e061c3
yassir.hrichi <yassir.hrichi@gmail.com>
2025-02-11 07:39:56 +0100

    Starting the mdx implementation

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { readMdxFile } from "./utils";
 
 export const metadata = {
     title: 'Blog',
@@ -6,6 +7,7 @@ export const metadata = {
   }
 
 export default function page() {
+  console.log(readMdxFile("today-i-learnt-22-01-2025.mdx"))
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
